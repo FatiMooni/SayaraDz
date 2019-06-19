8d1c4a4faa7d11faae3aafbe3af03390c9c153c9
FatiMooni <ff_abdiche@esi.dz>
2019-06-19 16:18:22 +0200

    added

--- app/src/main/java/com/example/sayaradzmb/customs/MenuClass.kt
+++ app/src/main/java/com/example/sayaradzmb/customs/MenuClass.kt
@@ -3,12 +3,14 @@ package com.example.sayaradzmb.customs
 import android.R
 import android.widget.Toast
 import android.view.MenuItem
-import androidclass MenuClass(private val position: Int) : PopupMenu.OnMenuItemClickListener {
+import android.widget.PopupMenu
+
+class MenuClass(private val position: Int) : PopupMenu.OnMenuItemClickListener {
 
     override fun onMenuItemClick(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
 
-           R.id.edit_annonce
+          // R.id.edit_annonce -> return true
         }
         return false
     }
